ee6a5dee8a0f85fc8cffe5a2cd380a98758ecaa1
Francisco Paiva <xicocaio@gmail.com>
2018-10-26 17:35:00 -0300

    Add code to verify if solution is valid to utils

--- app/heuristics/utils.py
+++ app/heuristics/utils.py
@@ -2,6 +2,25 @@ import pandas as pd
 from models.schedule import Schedule
 
 
+def is_valid(n_jobs, schedule=None):
+    start = schedule.start
+    early_seq = schedule.early_seq
+    tardy_seq = schedule.tardy_seq
+
+    # check if the solution satisfies the problem restrictions
+    dict_intersect = (set(early_seq.keys()) & set(tardy_seq.keys()))
+
+    if start < 0 or len(early_seq) + len(tardy_seq) != n_jobs or len(dict_intersect) > 0:
+        print('start < 0: {} < 0'.format(start))
+        print('len(early_seq) + len(tardy_seq) != n_jobs: {} != {}'.format(
+            len(early_seq) + len(tardy_seq), n_jobs))
+        print('len(dict_intersect) > 0: {}'.format(dict_intersect))
+
+        return False
+
+    return True
+
+
 def get_cost(schedule):
     early_cost = tardy_cost = 0
     d = end_of_job = schedule.d
--- app/tests/utils_test.py
+++ app/tests/utils_test.py
@@ -1,14 +1,8 @@
+from heuristics import utils
+
+
 def sequence_test(df=None, schedule=None):
-    start = schedule.start
-    early_seq = schedule.early_seq
-    tardy_seq = schedule.tardy_seq
-
-    # check if the solution satisfies the problem restrictions
-    dict_intersect = (set(early_seq.keys()) & set(tardy_seq.keys()))
-
-    if start < 0 or len(early_seq) + len(tardy_seq) != len(df.index) or len(dict_intersect) > 0:
-        print('start < 0: {} < 0'.format(start))
-        print('len(early_seq) + len(tardy_seq) == len(df.index): {} == {}'.format(
-            len(early_seq) + len(tardy_seq), len(df.index)))
-        print('len(dict_intersect) > 0: {}'.format(dict_intersect))
+    if utils.is_valid(len(df.index), schedule):
+        pass
+    else:
         raise ValueError('Solution is wrong')
